0038fd6b9b5717375b26a8d7946bf823c4ef1b86
Veesh Goldman <rabbiveesh@gmail.com>
2021-11-06 22:38:22 +0200

    fix it so it actually works

--- lib/DBIx/Class/SQLA2/Schema.pm
+++ lib/DBIx/Class/SQLA2/Schema.pm
@@ -7,12 +7,12 @@ sub connection {
   my ($self, @info) = @_;
   $self->next::method(@info);
   my $connect = sub {
-    shift->connect_call_rebase_sqlmaker($self->sqla2_subclass || 'DBIx::Class::SQLA2::Schema')
+    shift->connect_call_rebase_sqlmaker($self->sqla2_subclass || 'DBIx::Class::SQLA2')
   };
-  if (my $calls = $self->on_connect_call) {
-    $self->on_connect_call([ $connect, $calls ])
+  if (my $calls = $self->storage->on_connect_call) {
+    $self->storage->on_connect_call([ $connect, $calls ])
   } else {
-    $self->on_connect_call($connect)
+    $self->storage->on_connect_call($connect)
   }
   if ($self->sqla2_rebase_immediately) {
     $connect->($self->storage)
