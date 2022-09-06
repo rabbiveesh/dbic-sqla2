259487921d2ec4271db592994e6ac1ea4a8ba4f7
Roy Storey <kiwiroy@users.noreply.github.com>
2022-09-06 22:20:31 +1200

    Add a few tests and lines of pod

---
+++ t/lib/Local/Schema/Result/Artist.pm
@@ -0,0 +1,16 @@
+use utf8;
+
+package Local::Schema::Result::Artist;
+use base 'Local::Schema::Result';
+
+__PACKAGE__->table("artist");
+
+__PACKAGE__->add_columns(artistid => {data_type => 'integer', is_auto_increment => 1}, name => {data_type => 'text',},);
+
+__PACKAGE__->set_primary_key('artistid');
+
+__PACKAGE__->add_unique_constraint([qw( name )]);
+
+__PACKAGE__->has_many('albums' => 'Local::Schema::Result::Album', 'artistid');
+
+1;
---
+++ t/lib/Local/Schema/Result/Track.pm
@@ -0,0 +1,20 @@
+package Local::Schema::Result::Track;
+
+use warnings;
+use strict;
+
+use base qw( DBIx::Class::Core );
+
+__PACKAGE__->table('track');
+
+__PACKAGE__->add_columns(
+  trackid => {data_type => 'integer', is_auto_increment => 1},
+  albumid => {data_type => 'integer',},
+  title   => {data_type => 'text',},
+);
+
+__PACKAGE__->set_primary_key('trackid');
+
+__PACKAGE__->add_unique_constraint([qw( title albumid )]);
+
+__PACKAGE__->belongs_to('album' => 'Local::Schema::Result::Album', 'albumid');
