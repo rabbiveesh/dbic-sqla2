6e3f176a9da646562ffdf0e94f01fb8f8d55d465
Veesh Goldman <rabbiveesh@gmail.com>
2022-10-17 20:43:19 +0300

    Add perltidyrc (#2)
    * chore: run perltidy w/ our settings

--- t/lib/Local/Schema/Result/Album.pm
+++ t/lib/Local/Schema/Result/Album.pm
@@ -6,10 +6,11 @@ use base 'Local::Schema::Result';
 __PACKAGE__->table("album");
 
 __PACKAGE__->add_columns(
-  albumid  => {accessor  => 'albumid', data_type => 'integer', size => 16, is_nullable => 0, is_auto_increment => 1,},
-  artistid => {data_type => 'integer', size      => 16,        is_nullable => 0,},
-  title    => {data_type => 'varchar', size      => 256,       is_nullable => 0,},
-  rank     => {data_type => 'integer', size      => 16,        is_nullable => 0, default_value => 0,}
+  albumid =>
+      { accessor => 'albumid', data_type => 'integer', size => 16, is_nullable => 0, is_auto_increment => 1, },
+  artistid => { data_type => 'integer', size => 16,  is_nullable => 0, },
+  title    => { data_type => 'varchar', size => 256, is_nullable => 0, },
+  rank     => { data_type => 'integer', size => 16,  is_nullable => 0, default_value => 0, }
 );
 
 __PACKAGE__->set_primary_key('albumid');
