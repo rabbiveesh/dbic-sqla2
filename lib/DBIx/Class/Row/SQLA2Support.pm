476d14056ab2bcc606fb388da76b652dd7100dc4
Veesh Goldman <rabbiveesh@gmail.com>
2022-10-19 14:58:08 +0300

    fix: don't use postfix-deref

--- lib/DBIx/Class/Row/SQLA2Support.pm
+++ lib/DBIx/Class/Row/SQLA2Support.pm
@@ -10,7 +10,7 @@ sub new {
   my $new = $class->next::method($attrs);
   $new->{_sqla2_attrs} = { on_conflict => $on_conflict } if defined $on_conflict;
   if ($to_upsert) {
-    $to_upsert = { $attrs->%* };
+    $to_upsert = { %$attrs };
     my @pks = $new->result_source->primary_columns;
     delete @$to_upsert{@pks};
     $new->{_sqla2_attrs} = { on_conflict => { -target => \@pks, -set => $to_upsert }}
