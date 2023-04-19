7d55a8d0c26ad02abcd30e548e9fe665ffbadc74
Veesh Goldman <rabbiveesh@gmail.com>
2023-04-19 13:36:36 +0000

    fix: make -upsert more resilient by using `excluded.$_`
      * now it can respect the user's inflation settings on input!

--- lib/DBIx/Class/Row/SQLA2Support.pm
+++ lib/DBIx/Class/Row/SQLA2Support.pm
@@ -6,17 +6,18 @@ use parent 'DBIx::Class::Row';
 sub new {
   my ($class, $attrs) = @_;
   my $on_conflict = delete $attrs->{-on_conflict};
-  my $to_upsert = 1 if delete $attrs->{-upsert};
-  my $new = $class->next::method($attrs);
+  my $to_upsert   = 1 if delete $attrs->{-upsert};
+  my $new         = $class->next::method($attrs);
   $new->{_sqla2_attrs} = { on_conflict => $on_conflict } if defined $on_conflict;
   if ($to_upsert) {
     $to_upsert = { %$attrs };
     my @pks = $new->result_source->primary_columns;
     delete @$to_upsert{@pks};
-    $new->{_sqla2_attrs} = { on_conflict => { -target => \@pks, -set => $to_upsert }}
+    $new->{_sqla2_attrs}
+        = { on_conflict => { -target => \@pks, -set => { map +($_ => { -ident => "excluded.$_" }), keys $to_upsert->%* } } };
   }
 
-  return $new
+  return $new;
 }
 
 sub insert {
@@ -24,7 +25,7 @@ sub insert {
   my $extras = delete $self->{_sqla2_attrs};
   # this should allow relations to fail if they don't have a on_conflict defined
   local $self->result_source->storage->sql_maker->{_sqla2_insert_attrs} = $extras;
-  $self->next::method(@args)
+  $self->next::method(@args);
 }
 
 1
